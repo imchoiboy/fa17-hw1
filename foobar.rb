f83fe0d1dd3d5656cfee2aab78dbcfdf2dde2736
Matthew Choi <matthewchoi66@gmail.com>
2017-09-14 11:35:13 -0700

    hw 1 finished

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,18 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    arr = []
+    a.each do |element|
+    	element = Integer(element) + 2
+    	if element >= 10 or element % 2 != 0 or arr.include? element
+    		a.delete(element)
+    	else
+    		arr << element
+    		sum = sum + element
+    	end
+    end
+    sum
   end
 end
 
